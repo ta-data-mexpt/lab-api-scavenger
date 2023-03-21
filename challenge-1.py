cf37853c6cfe682eeebe5987ae2fd87e0c9c335d
LiLeon <tllt100@hotmail.com>
2023-03-21 15:33:40 -0600

    [Liliana_Leon]_Lab_Api_Scavenger
    ls

--- challenge-1.py
+++ challenge-1.py
@@ -1,1 +1,10 @@
 # enter your code below
+import configparser
+import time
+import requests
+
+config = configparser.ConfigParser()
+config.read('Ironhack/Config/config2.cfg')
+
+token = config['GITHUB']['token']
+headers = {'Authorization': 'token ' + token}
